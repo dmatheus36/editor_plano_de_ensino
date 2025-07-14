96d1f1052775413bc72b328f468b53fd72f932a0
dmatheus36 <43154075+dmatheus36@users.noreply.github.com>
2025-07-13 23:04:16 -0300

    Update CalendarioAtividades.java

--- trabalhoPOO/model/CalendarioAtividades.java
+++ trabalhoPOO/model/CalendarioAtividades.java
@@ -2,10 +2,38 @@ package model;
 
 public class CalendarioAtividades {
     private String descricaoConteudo;
-    private int data;
+    private String data;
     private int cargaHoraria;
 
-    public CalendarioAtividades(String descricaoConteudo, int data, int cargaHoraria) {
-        
+    public CalendarioAtividades(String descricaoConteudo, String data, int cargaHoraria) {
+        setDescricaoConteudo(descricaoConteudo);
+        setData(data);
+        setCargaHoraria(cargaHoraria);
     }
+
+    public String getDescricaoConteudo() {
+        return descricaoConteudo;
+    }
+
+    public void setDescricaoConteudo(String descricaoConteudo) {
+        this.descricaoConteudo = descricaoConteudo;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    
 }
