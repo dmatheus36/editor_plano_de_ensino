41725a0884230f0d27e0fd34497239ae97ad32b2
dmatheus36 <43154075+dmatheus36@users.noreply.github.com>
2025-07-18 21:49:17 -0300

    Create Usuario.java

---
+++ trabalhoPOO/model/Usuario.java
@@ -0,0 +1,10 @@
+package model;
+
+public interface Usuario {
+    public String getNome();
+    public void setNome(String nome);
+    public String getLogin();
+    public void setLogin(String login);
+    public String getSenha();
+    public void setSenha(String senha);
+}
